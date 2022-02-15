c4b2061d5b78af416ff5f192ba2064f0a6b8e62e
Tyler Wu <tylerwu2222@g.ucla.edu>
2022-02-14 17:36:08 -0800

    adding complete data

--- get_sentiment.py
+++ get_sentiment.py
@@ -4,8 +4,10 @@ from textblob import TextBlob
 
 # Read in dataset
 # df = pd.read_csv('tweet_data/model_test_data.csv')
-df = pd.read_csv('merged_test_data.csv')
-
+df = pd.read_csv('other_data/all_tweets.csv',
+    dtype={'text': 'str'})
+print(df.dtypes)
+print(df.columns)
 #print(df.shape)
 
 # Define empty lists to append to
@@ -18,12 +20,13 @@ for i in range(0, df.shape[0]):
 
 # Insert polarity and subjectivity as columns 
 # Might have to adjust 11 and 12 (I did that since model_test_data had 11 columns)
-df.insert(11, 'Polarity', polarity)
-df.insert(12, 'Subjectivity', subjectivity)
-
+df.insert(6, 'Polarity', polarity)
+df.insert(7, 'Subjectivity', subjectivity)
+df['Polarity'] = polarity
+df['Subjectivity'] = subjectivity
 #print(df.shape)
 
 # Write to a csv
-df.to_csv('merged_test_data_with_sentiment.csv')
+df.to_csv('all_tweets_with_sentiment.csv',index=False)
 
 
