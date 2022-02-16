dd8dd4600a08788df87e376fc984ccd92609709a
Matthew Paull <matthewpaull@MacBook-Pro.local>
2022-02-15 19:08:43 -0800

    adding merged data with CA/LA info

--- CA_column.R
+++ CA_column.R
@@ -1,13 +1,28 @@
 df <- read.csv('merged_data_with_sentiment.csv')
 
-length(which(df$location == 'Los Angeles, CA'))
-
 library(stringr)
 
-ca_ind <- numeric(0)
+la_ind <- character(nrow(df))
+ca_ind <- character(nrow(df))
 for (i in 1:nrow(df)) {
-  if (str_detect(df[i, 2], 'CA')) {
-    ca_ind <- c(ca_ind, i)
+  if (str_detect(df[i, 2], 'Los Angeles')) {
+    la_ind[i] <- 'Yes'
+    ca_ind[i] <- 'Yes'
+  } else if (str_detect(df[i, 2], 'CAMPO') | str_detect(df[i, 2], 'AMERICA') | str_detect(df[i, 2], 'CAMBRIDGE')){
+    la_ind[i] <- 'No'
+    ca_ind[i] <- 'No'
+  } else if (str_detect(df[i, 2], 'CA') | str_detect(df[i, 2], 'California')) {
+    la_ind[i] <- 'No'
+    ca_ind[i] <- 'Yes'
+  } else {
+    la_ind[i] <- 'No'
+    ca_ind[i] <- 'No'
   }
 }
+
+df['CA'] <- ca_ind
+df['LA'] <- la_ind
+
+write.csv(df, './merged_data_with_sentiment_and_CA_LA.csv', row.names = FALSE)
+
 
